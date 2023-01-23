c5860649a33c0a742268861b288254cc5741bc7b
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-23 23:33:57 +0300

    feat: implement mail service

---
+++ src/services/MailService.ts
@@ -0,0 +1,22 @@
+import { IMessage } from "../models/message-model";
+import UserModel from "../models/user-model";
+
+class MailService {
+  async getUserMessages(name: string) {
+    let user = await UserModel.findOne({ name });
+    if (!user) {
+      user = await UserModel.create({ name, messages: [] });
+    }
+    return { name: user.name, messages: user.messages };
+  }
+  async sendMessage(recipient: string, message: IMessage) {
+    let user = await UserModel.findOne({ name: recipient });
+    if (!user) {
+      user = await UserModel.create({ name: recipient, messages: [] });
+    }
+    await UserModel.updateOne({ name: recipient }, { $push: { messages: message } });
+    return { message };
+  }
+}
+
+export default new MailService();
